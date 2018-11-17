7d14c6e54d48a9973dd478f72e2acf49eeea2032
Adam Mescher <admescher@gmail.com>
2018-11-17 14:58:35 -0700

    Add h1 tag and text to make the page loading more obvious

--- pages/index.js
+++ pages/index.js
@@ -5,6 +5,7 @@ import Head from '../components/head';
 const Home = () => (
   <div>
     <Head title="Home" />
+    <h1>If you're reading this, your project is at least partially working!</h1>
   </div>
 )
 
